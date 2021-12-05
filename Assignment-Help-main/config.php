62160b444caef84164ac40ef9ffbe6bfecfaca24
Moses Mbadi <mosesmbadi@gmail.com>
2021-12-05 16:48:49 +0300

    Some cleaning

--- Assignment-Help-main/config.php
+++
@@ -1,8 +0,0 @@
-<?php  
-	$db_user = "webassig_webassign1";
-	$db_pass = "-wcDi1x~Sn0-";
-	$db_name = "webassig_webassignment";
-
-	$db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
